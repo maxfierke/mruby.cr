b53474eb8b97aad3b39438f47866f9020f0858ed
Max Fierke <max@maxfierke.com>
2019-10-12 15:08:02 -0500

    Add CI workflow

--- spec/mruby_spec.cr
+++ spec/mruby_spec.cr
@@ -2,6 +2,7 @@ require "./spec_helper"
 
 describe "LibMRuby" do
   it "works" do
+    # TODO: Work out a way to observe the output here.
     mrb = LibMRuby.mrb_open
     code = "5.times { puts 'mruby is awesome!' }"
     LibMRuby.mrb_load_string(mrb, code)
